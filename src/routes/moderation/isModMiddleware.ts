4c423076312d158d9ad307138d3727437d81652a
Supertiger <saawalkhn@googlemail.com>
2022-11-03 18:01:26 +0000

    allow admins to access the moderation pane

--- src/routes/moderation/isModMiddleware.ts
+++ src/routes/moderation/isModMiddleware.ts
@@ -5,7 +5,8 @@ import { generateError } from '../../common/errorHandler';
 export function isModMiddleware(req: Request, res: Response, next: NextFunction){
   const badges = req.accountCache.user.badges;
   const isCreator = hasBit(badges, USER_BADGES.CREATOR.bit);
-  if (!isCreator) {
+  const isAdmin = hasBit(badges, USER_BADGES.ADMIN.bit);
+  if (!isCreator && !isAdmin) {
     return res.status(403).json(generateError('Admin access only!'));
   }
   next();
