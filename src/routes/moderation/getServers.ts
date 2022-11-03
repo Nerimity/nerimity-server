658201c1c36da90d21d60bb0f79b7fb97ebea9f7
Supertiger <saawalkhn@googlemail.com>
2022-11-03 17:37:32 +0000

    Added limit option

--- src/routes/moderation/getServers.ts
+++ src/routes/moderation/getServers.ts
@@ -18,13 +18,19 @@ export function getServers(Router: Router) {
 async function route (req: Request, res: Response) {
   const after = req.query.after as string | undefined;
 
+  let limit = parseInt((req.query.limit || '30') as string);
+
+  if (limit > 30) {
+    limit = 30;
+  }
+
 
   const users = await prisma.server.findMany({
     orderBy: {
       createdAt: 'desc'
     },
     ...(after ? {skip: 1} : undefined),
-    take: 30,
+    take: limit,
     ...(after ? {cursor: { id: after }} : undefined),
     select: {name: true, hexColor: true, id: true, createdAt: true, createdBy: {select: {id: true, username: true, tag: true}}}
   });
