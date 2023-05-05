9e4b180734edd2201378b82d5cea22c2c0ac9ab0
Supertiger <saawalkhn@googlemail.com>
2023-05-05 21:26:44 +0100

    fix: :bug: Added validation for explore server bump and explore server join routes

--- src/routes/explore/exploreServerBump.ts
+++ src/routes/explore/exploreServerBump.ts
@@ -1,15 +1,27 @@
 import { Request, Response, Router } from 'express';
 import { authenticate } from '../../middleware/authenticate';
 import { bumpPublicServer } from '../../services/Explore';
+import { param } from 'express-validator';
+import { customExpressValidatorResult } from '../../common/errorHandler';
 
 export function exploreServerBump(Router: Router) {
   Router.post('/explore/servers/:serverId/bump', 
     authenticate(),
+    param('serverId')
+      .not().isEmpty().withMessage('serverId is required.')
+      .isString().withMessage('serverId must be a string.')
+      .isLength({ min: 3, max: 320}).withMessage('serverId must be between 3 and 320 characters long.'),
     route
   );
 }
 
 async function route (req: Request, res: Response) {
+
+  const validateError = customExpressValidatorResult(req);
+  if (validateError) {
+    return res.status(400).json(validateError);
+  }
+
   const { serverId } = req.params;
   const [server, error] = await bumpPublicServer(serverId);
 
--- src/routes/explore/exploreServerJoin.ts
+++ src/routes/explore/exploreServerJoin.ts
@@ -1,15 +1,26 @@
 import { Request, Response, Router } from 'express';
 import { authenticate } from '../../middleware/authenticate';
 import { joinPublicServer } from '../../services/Explore';
+import { param } from 'express-validator';
+import { customExpressValidatorResult } from '../../common/errorHandler';
 
 export function exploreServerJoin(Router: Router) {
   Router.post('/explore/servers/:serverId/join', 
     authenticate(),
+    param('serverId')
+      .not().isEmpty().withMessage('serverId is required.')
+      .isString().withMessage('serverId must be a string.')
+      .isLength({ min: 3, max: 320}).withMessage('serverId must be between 3 and 320 characters long.'),
     route
   );
 }
 
 async function route (req: Request, res: Response) {
+  const validateError = customExpressValidatorResult(req);
+  if (validateError) {
+    return res.status(400).json(validateError);
+  }
+
   const { serverId } = req.params;
   const [server, error] = await joinPublicServer(req.accountCache.user.id, serverId);
   if (error) {
