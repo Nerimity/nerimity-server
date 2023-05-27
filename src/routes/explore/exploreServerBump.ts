fb843f4b20cd93c1ffd8755bc1690fab91158b0d
Supertigerr <saawalkhn@googlemail.com>
2023-05-27 20:11:50 +0100

    added captcha to bump servers

--- src/routes/explore/exploreServerBump.ts
+++ src/routes/explore/exploreServerBump.ts
@@ -1,12 +1,16 @@
 import { Request, Response, Router } from 'express';
 import { authenticate } from '../../middleware/authenticate';
 import { bumpPublicServer } from '../../services/Explore';
-import { param } from 'express-validator';
-import { customExpressValidatorResult } from '../../common/errorHandler';
+import { body, param } from 'express-validator';
+import { customExpressValidatorResult, generateError } from '../../common/errorHandler';
+import { turnstileVerify } from '../../common/turnstileVerify';
 
 export function exploreServerBump(Router: Router) {
   Router.post('/explore/servers/:serverId/bump', 
     authenticate(),
+    body('token')   
+      .isString().withMessage('Token must be a string.')
+      .isLength({ min: 1, max: 5000 }).withMessage('Token must be between 1 and 5000 characters long.'),
     param('serverId')
       .not().isEmpty().withMessage('serverId is required.')
       .isString().withMessage('serverId must be a string.')
@@ -20,6 +24,12 @@ async function route (req: Request, res: Response) {
   const validateError = customExpressValidatorResult(req);
   if (validateError) {
     return res.status(400).json(validateError);
+  }
+
+  const validToken = await turnstileVerify(req.body.token);
+
+  if (!validToken) {
+    return res.status(401).json(generateError('Invalid captcha! Please try again.', 'token'));
   }
 
   const { serverId } = req.params;
