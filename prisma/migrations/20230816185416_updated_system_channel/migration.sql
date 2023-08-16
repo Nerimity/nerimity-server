098a9c930aec3f8f3353b5ccccda9906929cbf32
SupertigerDev <saawalkhn@googlemail.com>
2023-08-16 20:45:47 +0100

    fixed migration

--- prisma/migrations/20230816185416_updated_system_channel/migration.sql
+++ prisma/migrations/20230816185416_updated_system_channel/migration.sql
@@ -4,6 +4,11 @@
   - A unique constraint covering the columns `[systemChannelId]` on the table `Server` will be added. If there are existing duplicate values, this will fail.
 
 */
+UPDATE "Server"
+SET "systemChannelId" = NULL
+WHERE "systemChannelId" NOT IN (SELECT id FROM "Channel");
+
+
 -- CreateIndex
 CREATE UNIQUE INDEX "Server_systemChannelId_key" ON "Server"("systemChannelId");
 
