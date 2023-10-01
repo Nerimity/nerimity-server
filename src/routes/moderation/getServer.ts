98102a35abd938dea1974b5c554fc45140eb8667
SupertigerDev <saawalkhn@googlemail.com>
2023-10-01 18:56:30 +0100

    moderation: Fixed get server route

--- src/routes/moderation/getServer.ts
+++ src/routes/moderation/getServer.ts
@@ -2,9 +2,10 @@ import { Request, Response, Router } from 'express';
 import { prisma } from '../../common/database';
 import { authenticate } from '../../middleware/authenticate';
 import { isModMiddleware } from './isModMiddleware';
+import { isExpired } from '../../services/User';
 
 export function getServer(Router: Router) {
-  Router.get('/moderation/servers/:serverId', 
+  Router.get('/moderation/servers/:serverId',
     authenticate(),
     isModMiddleware,
     route
@@ -12,13 +13,33 @@ export function getServer(Router: Router) {
 }
 
 
-async function route (req: Request, res: Response) {
+async function route(req: Request, res: Response) {
   const serverId = req.params.serverId;
 
   const server = await prisma.server.findFirst({
-    where: {id: serverId},
-    select: { _count: {select: {serverMembers: true}}, banner: true, name: true, verified: true, hexColor: true, id: true, createdAt: true, createdBy: {select: {id: true, username: true, tag: true}}, avatar: true}
+    where: { id: serverId },
+    select: {
+      _count: { select: { serverMembers: true } }, banner: true, name: true, verified: true, hexColor: true, id: true, createdAt: true, createdBy: {
+        select: {
+          id: true,
+          username: true,
+          joinedAt: true,
+          tag: true,
+          hexColor: true,
+          avatar: true,
+          suspension: true,
+        }
+      }, avatar: true
+    }
   });
+
+
+
+  if (server?.createdBy.suspension?.expireAt && isExpired(server.createdBy.suspension.expireAt)) {
+    server.createdBy.suspension = null;
+  }
+
 
   res.json(server);
 }
+
