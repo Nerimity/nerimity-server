5c17c27c76de25a49cf91b96d0410160c6c50cd3
SupertigerDev <saawalkhn@googlemail.com>
2023-10-17 22:00:04 +0100

    Removed un-needed entries in tsup config

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
-  entry: ['src/index.ts', 'src/common/ServerEventNames.ts', 'src/common/ClientEventNames.ts'],
+  entry: ['src/index.ts'],
   splitting: false,
   sourcemap: true,
   clean: true,
