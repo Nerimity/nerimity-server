08d09ef15a8e5300e550c7e0dcd78ad12ef28e42
Supertiger <saawalkhn@googlemail.com>
2023-11-20 20:05:21 +0000

    Add Ticket channel cache

--- src/cache/CacheKeys.ts
+++ src/cache/CacheKeys.ts
@@ -1,18 +1,16 @@
 const PREFIX = 'NERIMITY_';
 export const ACCOUNT_CACHE_KEY_STRING = (userId: string) => `${PREFIX}ACCOUNT:${userId}`;
 
-
-
 export const GOOGLE_ACCESS_TOKEN = (userId: string) => `${PREFIX}GOOGLE_ACCESS_TOKEN:${userId}`;
 
 export const SERVER_KEY_STRING = (serverId: string) => `${PREFIX}SERVER:${serverId}`;
 
 export const SERVER_CHANNEL_KEY_STRING = (channelId: string) => `${PREFIX}SERVER_CHANNEL:${channelId}`;
 export const DM_CHANNEL_KEY_STRING = (channelId: string) => `${PREFIX}DM_CHANNEL:${channelId}`;
+export const TICKET_CHANNEL_KEY_STRING = (channelId: string) => `${PREFIX}TICKET_CHANNEL:${channelId}`;
 export const INBOX_KEY_STRING = (channelId: string, userId: string) => `${PREFIX}INBOX:${userId}:${channelId}`;
 
 export const SERVER_MEMBERS_KEY_HASH = (serverId: string) => `${PREFIX}SERVER_MEMBERS:${serverId}`;
-
 
 export const CONNECTED_SOCKET_ID_KEY_SET = (userId: string) => `${PREFIX}SOCKET_USER_CONNECTED:${userId}`;
 export const CONNECTED_USER_ID_KEY_STRING = (socketId: string) => `${PREFIX}SOCKET_USER_ID:${socketId}`;
@@ -21,7 +19,6 @@ export const VOICE_USERS_KEY_HASH = (channelId: string) => `${PREFIX}VOICE_USERS
 export const VOICE_USER_CHANNEL_ID_SET = (userId: string) => `${PREFIX}VOICE_USER_CHANNEL_ID:${userId}`
 
 export const USER_PRESENCE_KEY_STRING = (userId: string) => `${PREFIX}USER_PRESENCE:${userId}`;
-
 
 export const RATE_LIMIT_KEY_STRING = (id: string) => `${PREFIX}RATE_LIMIT:${id}`;
 
