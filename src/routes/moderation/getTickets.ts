355ae3138e057720534972950044b338be1c98b4
Supertiger <saawalkhn@googlemail.com>
2023-11-25 12:53:52 +0000

    Add status filter to get tickets moderation route

--- src/routes/moderation/getTickets.ts
+++ src/routes/moderation/getTickets.ts
@@ -2,11 +2,18 @@ import { Request, Response, Router } from 'express';
 import { prisma } from '../../common/database';
 import { authenticate } from '../../middleware/authenticate';
 import { isModMiddleware } from './isModMiddleware';
+import { TicketStatus } from '../../services/Ticket';
 export function getTickets(Router: Router) {
   Router.get('/moderation/tickets', authenticate(), isModMiddleware, route);
 }
 
 async function route(req: Request, res: Response) {
+  let status: undefined | TicketStatus;
+
+  if (typeof req.query.status === 'string') {
+    status = parseInt(req.query.status) as TicketStatus;
+  }
+
   const after = req.query.after
     ? parseInt(req.query.after as string)
     : undefined;
@@ -20,6 +27,7 @@ async function route(req: Request, res: Response) {
     orderBy: {
       lastUpdatedAt: 'desc',
     },
+    ...(status !== undefined ? { where: { status } } : undefined),
     ...(after ? { skip: 1 } : undefined),
     take: limit,
     ...(after ? { cursor: { id: after } } : undefined),
