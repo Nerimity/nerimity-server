2cad5efb51fe554631e1119c7bf7365bc3afaf40
Supertiger <saawalkhn@googlemail.com>
2024-03-08 18:24:00 +0000

    Fixed bugs

--- src/common/badWords.ts
+++ src/common/badWords.ts
@@ -4,17 +4,27 @@ const goodWords = ['I love myself', 'I love you', "I'm a good person!", 'uwu <3'
 
 export const replaceBadWords = (message: string) => {
   if (BadWords.includes(message.toLowerCase().trim())) {
-    const randomGoodWord = goodWords[Math.floor(Math.random() * goodWords.length)]!;
+    const randomGoodWord = goodWords[Math.floor(Math.random() * goodWords.length)];
     return randomGoodWord;
   }
 
-  return message
-    .split(' ')
-    .map((word) => {
-      if (BadWords.includes(word.toLowerCase())) {
-        return '#'.repeat(word.length);
-      }
-      return word;
-    })
-    .join(' ');
+  let cleanMessage = ' ' + message + ' ';
+
+  for (let i = 0; i < BadWords.length; i++) {
+    const badWord = BadWords[i]!;
+    const hashes = createHashes(badWord);
+    cleanMessage = cleanMessage.replaceAll(` ${badWord} `, ` ${hashes} `);
+    cleanMessage = cleanMessage.replaceAll(`${badWord} `, `${hashes} `);
+    cleanMessage = cleanMessage.replaceAll(` ${badWord}`, ` ${hashes}`);
+  }
+  return cleanMessage.trim();
+};
+
+const createHashes = (badWord: string) => {
+  let str = '';
+  for (let i = 0; i < badWord.length; i++) {
+    const char = badWord[i];
+    str += char === ' ' ? ' ' : '#';
+  }
+  return str;
 };
