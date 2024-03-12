788b356bc8e9a2474ba7a181b93afcc081a9e771
Supertiger <saawalkhn@googlemail.com>
2024-03-12 11:28:13 +0000

    Bypass admin

--- src/routes/posts/postsDiscover.ts
+++ src/routes/posts/postsDiscover.ts
@@ -2,6 +2,7 @@ import { Request, Response, Router } from 'express';
 import { authenticate } from '../../middleware/authenticate';
 import { rateLimit } from '../../middleware/rateLimit';
 import { fetchPosts } from '../../services/Post';
+import { isUserAdmin } from '../../common/Bitwise';
 
 export function postsDiscover(Router: Router) {
   Router.get(
@@ -24,9 +25,13 @@ interface Query {
 
 async function route(req: Request, res: Response) {
   const query = req.query as Query;
+
+  const isAdmin = isUserAdmin(req.userCache.badges);
+
   const posts = await fetchPosts({
     hideIfBlockedByMe: true,
     withReplies: false,
+    bypassBlocked: isAdmin,
     requesterUserId: req.userCache.id,
     limit: query.limit ? parseInt(query.limit) : undefined,
     afterId: query.afterId,
