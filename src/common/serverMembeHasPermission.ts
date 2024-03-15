153bd59905092b65288f1534e01e25865cd2d563
Supertiger <saawalkhn@googlemail.com>
2024-03-15 09:50:08 +0000

    Add default role to checking permission

--- src/common/serverMembeHasPermission.ts
+++ src/common/serverMembeHasPermission.ts
@@ -1,15 +1,26 @@
 import { Bitwise, ROLE_PERMISSIONS, hasBit } from './Bitwise';
 
-export const serverMemberHasPermission = (permission: Bitwise, member: { roleIds: string[] }, serverRoles: { permissions: number; id: string }[], ignoreAdmin = false) => {
-  const memberRoles = member.roleIds.map((id) => serverRoles.find((role) => role.id === id));
+interface serverMemberHasPermissionOpts {
+  permission: Bitwise;
+  member: { roleIds: string[] };
+  serverRoles: { permissions: number; id: string }[];
+  ignoreAdmin?: boolean;
+  defaultRoleId: string;
+}
+
+export const serverMemberHasPermission = (opts: serverMemberHasPermissionOpts) => {
+  const memberRoles = opts.member.roleIds.map((id) => opts.serverRoles.find((role) => role.id === id));
+
+  const defaultRole = opts.serverRoles.find((role) => role.id === opts.defaultRoleId);
+  if (defaultRole) memberRoles.push(defaultRole);
 
   for (let i = 0; i < memberRoles.length; i++) {
     const role = memberRoles[i];
     if (!role) continue;
-    if (!ignoreAdmin) {
+    if (!opts.ignoreAdmin) {
       if (hasBit(role.permissions, ROLE_PERMISSIONS.ADMIN.bit)) return true;
     }
-    const hasPerm = hasBit(role.permissions, permission.bit);
+    const hasPerm = hasBit(role.permissions, opts.permission.bit);
     if (hasPerm) return true;
   }
   return false;
