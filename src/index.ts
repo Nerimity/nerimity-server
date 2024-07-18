130f4e0deb3c8a55b8e68f2c15b0ba20373040d8
Supertiger <saawalkhn@googlemail.com>
2024-07-18 22:17:12 +0100

    made post images large

--- src/index.ts
+++ src/index.ts
@@ -62,15 +62,16 @@ app.use(express.urlencoded({ extended: false, limit: '20MB' }));
 
 app.use(userIP);
 
-const makeOpenGraph = (opts: { url: string; title: string; image?: string; description: string }) => {
+const makeOpenGraph = (opts: { largeImage?: boolean; url: string; title: string; image?: string; description: string }) => {
   const siteName = `<meta content="Nerimity" property="og:site_name" />`;
   const type = `<meta content="article" property="og:type" />`;
   const url = `<meta content="${opts.url}" property="og:url" />`;
   const title = `<meta content="${opts.title}" property="og:title" />`;
   const description = `<meta content="${opts.description}" property="og:description" />`;
   const image = opts.image ? `<meta content="${opts.image}" property="og:image" />` : '';
+  const largeImage = opts.largeImage ? `<meta name="twitter:card" content="summary_large_image">` : '';
 
-  return `<!DOCTYPE html><html><head>${siteName}${type}${url}${title}${description}${image}</head></html>`;
+  return `<!DOCTYPE html><html><head>${siteName}${type}${url}${title}${description}${largeImage}${image}</head></html>`;
 };
 
 app.get('/api/og/*', rateLimit({ name: 'og', useIP: true, requests: 30, restrictMS: 60 * 1000 }), async (req, res, next) => {
@@ -102,6 +103,7 @@ app.get('/api/og/*', rateLimit({ name: 'og', useIP: true, requests: 30, restrict
     url: `https://nerimity.com/app?postId=${postId}`,
     title: `${post.createdBy.username} on Nerimity`,
     description: post.content || '',
+    largeImage: true,
     image: attachmentPath ? `${env.NERIMITY_CDN}${attachmentPath}` : undefined,
   });
 
