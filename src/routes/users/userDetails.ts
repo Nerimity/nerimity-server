9e0ac09938d8ef89bb7d0cfac3c2ba2cbae9152f
Administrator <81298350+Deutscher775@users.noreply.github.com>
2025-02-20 19:36:42 +0100

    allow bots to use /users/:userid

--- src/routes/users/userDetails.ts
+++ src/routes/users/userDetails.ts
@@ -5,7 +5,7 @@ import { authenticate } from '../../middleware/authenticate';
 import { getUserDetails } from '../../services/User/User';
 
 export function userDetails(Router: Router) {
-  Router.get('/users/:userId?', authenticate({ allowNoToken: true }), param('userId').isString().withMessage('Invalid userId.').isLength({ min: 1, max: 320 }).withMessage('userId must be between 1 and 320 characters long.').optional(), route);
+  Router.get('/users/:userId?', authenticate({ allowNoToken: true, allowBot: true }), param('userId').isString().withMessage('Invalid userId.').isLength({ min: 1, max: 320 }).withMessage('userId must be between 1 and 320 characters long.').optional(), route);
 }
 
 async function route(req: Request, res: Response) {
