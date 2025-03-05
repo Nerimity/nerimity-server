2f4e72b314fc8434a085adf4e60d6fc2c371b7a0
Supertiger <saawalkhn@googlemail.com>
2025-03-05 12:36:59 +0000

    shorten name

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,14 +1,14 @@
 module.exports = {
   apps: [
     {
-      name: 'nerimity-api',
+      name: 'neri-api',
       script: 'npm',
       args: 'run start-api',
       watch: true,
       ignore_watch: ['node_modules', 'dist'],
     },
     {
-      name: 'nerimity-ws',
+      name: 'neri-ws',
       script: 'npm',
       args: 'run start-ws',
       watch: true,
