553cc812294e5de10e6375c4209577f4af4cb444
Supertiger <saawalkhn@googlemail.com>
2025-03-30 21:28:04 +0100

    fix bug

--- src/routes/explore/exploreServerUpdate.ts
+++ src/routes/explore/exploreServerUpdate.ts
@@ -1,9 +1,6 @@
 import { Request, Response, Router } from 'express';
 import { body, param } from 'express-validator';
-import {
-  customExpressValidatorResult,
-  generateError,
-} from '../../common/errorHandler';
+import { customExpressValidatorResult, generateError } from '../../common/errorHandler';
 import { authenticate } from '../../middleware/authenticate';
 import { rateLimit } from '../../middleware/rateLimit';
 import { serverMemberVerification } from '../../middleware/serverMemberVerification';
@@ -19,19 +16,8 @@ export function exploreServerUpdate(Router: Router) {
       restrictMS: 10000,
       requests: 15,
     }),
-    param('serverId')
-      .not()
-      .isEmpty()
-      .withMessage('serverId is required.')
-      .isString()
-      .withMessage('serverId must be a string.')
-      .isLength({ min: 3, max: 320 })
-      .withMessage('serverId must be between 3 and 320 characters long.'),
-    body('description')
-      .isString()
-      .withMessage('Description must be a string!')
-      .isLength({ min: 1, max: 150 })
-      .withMessage('Description length must be between 1 and 150 characters.'),
+    param('serverId').not().isEmpty().withMessage('serverId is required.').isString().withMessage('serverId must be a string.').isLength({ min: 3, max: 320 }).withMessage('serverId must be between 3 and 320 characters long.'),
+    body('description').isString().withMessage('Description must be a string!').isLength({ min: 1, max: 150 }).withMessage('Description length must be between 1 and 150 characters.'),
     route
   );
 }
@@ -42,16 +28,15 @@ async function route(req: Request, res: Response) {
     return res.status(400).json(validateError);
   }
 
-  if (req.serverCache.createdById !== req.userCache.id) {
-    return res
-      .status(401)
-      .json(generateError('Only the server creator modify this.'));
+  if (req.userCache.shadowBanned) {
+    return res.status(403).json(generateError('Something went wrong. Try again later.'));
   }
 
-  const [publicServer, error] = await updatePublicServer(
-    req.params.serverId,
-    req.body.description
-  );
+  if (req.serverCache.createdById !== req.userCache.id) {
+    return res.status(401).json(generateError('Only the server creator modify this.'));
+  }
+
+  const [publicServer, error] = await updatePublicServer(req.params.serverId, req.body.description);
   if (error) {
     return res.status(400).json(error);
   }
