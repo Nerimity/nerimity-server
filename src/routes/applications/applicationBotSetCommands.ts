666b3430964757de7c182fbcbc8824ed0a35e154
Supertiger <saawalkhn@googlemail.com>
2025-04-05 09:39:59 +0100

    add length

--- src/routes/applications/applicationBotSetCommands.ts
+++ src/routes/applications/applicationBotSetCommands.ts
@@ -16,11 +16,11 @@ export function applicationBotSetCommands(Router: Router) {
       })
       .withMessage('Commands length must be less than  100 strings.'),
 
-    body('commands.*.name').isString().withMessage('Command name must be a string!').notEmpty().withMessage('Command name is required!'),
+    body('commands.*.name').isString().withMessage('Command name must be a string!').notEmpty().withMessage('Command name is required!').isLength({ min: 1, max: 100 }).withMessage('Command name length must be between 1 and 100 characters.'),
 
-    body('commands.*.description').isString().withMessage('Command description must be a string!').optional(true),
+    body('commands.*.description').isString().withMessage('Command description must be a string!').isLength({ min: 0, max: 100 }).withMessage('Command description length must be less than or equal to 100 characters').optional(true),
 
-    body('commands.*.args').isString().withMessage('Command args must be a string!').optional(true),
+    body('commands.*.args').isString().withMessage('Command args must be a string!').isLength({ min: 0, max: 100 }).withMessage('Command args length must be less than or equal to 100 characters').optional(true),
 
     authenticate(),
     rateLimit({
