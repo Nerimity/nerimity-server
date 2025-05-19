99228e683b0ee16a5885b1d5bb3a8cdabba292c3
Supertiger <saawalkhn@googlemail.com>
2025-05-19 09:27:18 +0100

    fix bugs with external embed avatar cache

--- src/services/User/UserManagement.ts
+++ src/services/User/UserManagement.ts
@@ -481,8 +481,8 @@ export const getExternalEmbed = async (opts: { serverId?: string; userId?: strin
           const avatarEtxName = m.user.avatar ? path.extname(m.user.avatar) : undefined;
           return {
             ...{
-              avatar: m.user.avatar ? encodeURIComponent(encrypt(m.user.avatar, env.EXTERNAL_EMBED_SECRET)) + avatarEtxName : null,
-              banner: m.user.banner ? encodeURIComponent(encrypt(m.user.banner, env.EXTERNAL_EMBED_SECRET)) + bannerExtName : null,
+              avatar: m.user.avatar ? encodeURIComponent(encrypt(m.user.avatar, env.EXTERNAL_EMBED_SECRET)) + '/avatar' + avatarEtxName : null,
+              banner: m.user.banner ? encodeURIComponent(encrypt(m.user.banner, env.EXTERNAL_EMBED_SECRET)) + '/banner' + bannerExtName : null,
               hexColor: m.user.hexColor,
               username: m.nickname || m.user.username,
               id: i,
