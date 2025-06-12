b567ee80c52c7322ba0a66e0c5edeb986ca92b67
Supertiger <saawalkhn@googlemail.com>
2025-06-12 10:35:56 +0100

    Add queue for on voice signal

--- src/socket/events/onVoiceSignal.ts
+++ src/socket/events/onVoiceSignal.ts
@@ -1,8 +1,11 @@
-import { Socket } from "socket.io";
-import { getVoiceUserByUserId } from "../../cache/VoiceCache";
-import { getUserIdBySocketId } from "../../cache/UserCache";
-import { getIO } from "../socket";
-import { VOICE_SIGNAL_RECEIVED } from "../../common/ClientEventNames";
+import { Socket } from 'socket.io';
+import { getVoiceUserByUserId } from '../../cache/VoiceCache';
+import { getUserIdBySocketId } from '../../cache/UserCache';
+import { getIO } from '../socket';
+import { VOICE_SIGNAL_RECEIVED } from '../../common/ClientEventNames';
+import env from '../../common/env';
+import { createQueue } from '@nerimity/mimiqueue';
+import { redisClient } from '../../common/redis';
 
 interface Payload {
   channelId: string;
@@ -10,16 +13,33 @@ interface Payload {
   toUserId: string;
 }
 
+export const queue = createQueue({
+  name: 'joinVoice',
+  prefix: env.TYPE,
+  redisClient: redisClient,
+});
+
 export async function onVoiceSignal(socket: Socket, payload: Payload) {
+  await queue.add(
+    async () => {
+      await handleVoiceSignal(socket, payload).catch((err) => {
+        console.error(err);
+      });
+    },
+    { groupName: payload.channelId }
+  );
+}
+
+const handleVoiceSignal = async (socket: Socket, payload: Payload) => {
   const userId = await getUserIdBySocketId(socket.id);
   if (!userId) return;
 
   // am i in voice channel?
-  const voiceUser = await getVoiceUserByUserId(userId)
+  const voiceUser = await getVoiceUserByUserId(userId);
   if (voiceUser?.socketId !== socket.id) return;
 
   // is recipient in voice channel?
-  const recipientVoiceUser = await getVoiceUserByUserId(payload.toUserId)
+  const recipientVoiceUser = await getVoiceUserByUserId(payload.toUserId);
   if (!recipientVoiceUser) return;
 
   // are we in the same voice channel?
@@ -31,8 +51,6 @@ export async function onVoiceSignal(socket: Socket, payload: Payload) {
   getIO().to(recipientVoiceUser.socketId).emit(VOICE_SIGNAL_RECEIVED, {
     fromUserId: userId,
     channelId: payload.channelId,
-    signal: payload.signal
-  })
-
-
-}
+    signal: payload.signal,
+  });
+};
