f66504694b0b77efb86875133c77d69ff0b51bd4
Supertiger <saawalkhn@googlemail.com>
2025-09-10 17:46:21 +0100

    update prisma & use rust free driver

--- src/common/database.ts
+++ src/common/database.ts
@@ -1,10 +1,15 @@
 import { PrismaClient, Prisma } from '@prisma/client';
 import { logger } from './pino';
 import { Log } from './Log';
+import { PrismaPg } from '@prisma/adapter-pg';
+import env from './env';
 
 // import env from './env';
 
+const adapter = new PrismaPg({ connectionString: env.DATABASE_URL });
+
 export const prisma = new PrismaClient({
+  adapter,
   // log: ['error', 'warn', 'info', 'query'],
   log: [
     'warn',
