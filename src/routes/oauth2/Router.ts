0941f0ca0494ed38cf810687e70bb107ad8ce764
Supertiger <saawalkhn@googlemail.com>
2025-11-26 13:58:30 +0000

    add oauth2TokenRoute to Oauth2Router

--- src/routes/oauth2/Router.ts
+++ src/routes/oauth2/Router.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import { oauth2AuthorizeRoute } from './oauth2AuthorizeRoute';
+import { oauth2TokenRoute } from './oauth2TokenRoute';
 
 const Oauth2Router = Router();
 
 oauth2AuthorizeRoute(Oauth2Router);
+oauth2TokenRoute(Oauth2Router);
 
 export { Oauth2Router };
