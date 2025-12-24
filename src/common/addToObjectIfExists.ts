cce52b88092d7088b7964d6f580f0dfbf1911d23
Supertiger <saawalkhn@googlemail.com>
2025-12-24 13:53:32 +0000

    fix: update comment for clarity on function behavior

--- src/common/addToObjectIfExists.ts
+++ src/common/addToObjectIfExists.ts
@@ -1,3 +1,4 @@
+// it is important that this only works returns when the value is not undefined, and not null
 export function addToObjectIfExists(key: string, value: any, customValue?: any) {
   if (value === undefined) return;
   return { [key]: customValue || value };
