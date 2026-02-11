b3f779286ac99ec4426c8fc96ac1412af1650601
Supertiger <saawalkhn@googlemail.com>
2026-02-11 10:07:10 +0000

    fix: replace googleapis with google-auth-library for OAuth2 client implementation

--- src/common/GoogleOAuth2Client.ts
+++ src/common/GoogleOAuth2Client.ts
@@ -1,12 +1,8 @@
-import { google } from 'googleapis';
+import { OAuth2Client } from 'google-auth-library';
 import env from '../common/env';
 
 export const googleOAuth2Client = (refreshToken?: string) => {
-  const GoogleOAuth2Client = new google.auth.OAuth2(
-    env.GOOGLE_CLIENT_ID,
-    env.GOOGLE_CLIENT_SECRET,
-    env.GOOGLE_REDIRECT_URL
-  );
+  const GoogleOAuth2Client = new OAuth2Client(env.GOOGLE_CLIENT_ID, env.GOOGLE_CLIENT_SECRET, env.GOOGLE_REDIRECT_URL);
 
   if (refreshToken) {
     GoogleOAuth2Client.setCredentials({ refresh_token: refreshToken });
