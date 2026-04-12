80fe0b3e47ee9c493316b3e70cf0b3624cd2e205
Supertiger <saawalkhn@googlemail.com>
2026-04-12 15:31:31 +0100

    fix bugs

--- src/routes/users/Router.ts
+++ src/routes/users/Router.ts
@@ -30,13 +30,8 @@ import { userExternalEmbedDelete } from './userExternalEmbedDelete';
 import { userExternalEmbedGet } from './userExternalEmbedGet';
 import { userExternalEmbedRawGet } from './userExternalEmbedRawGet';
 import { userInventoryGet } from './userInventoryGet';
-import { userClanDelete } from './userclanDelete';
-import { userClanUpdate } from './userClanUpdate';
 
 const UsersRouter = Router();
-
-userClanUpdate(UsersRouter);
-userClanDelete(UsersRouter);
 
 userInventoryGet(UsersRouter);
 
